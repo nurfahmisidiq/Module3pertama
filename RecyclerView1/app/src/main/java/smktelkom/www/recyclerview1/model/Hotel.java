38d23e220e9a4a7af6bae35dca72f57b9c17d423
nur fahmi sidiq <nur_fahmi_26rpl@student.smktelkom-mlg.sch.id>
2019-03-13 16:43:14 +0700

    selesai

---
+++ RecyclerView1/app/src/main/java/smktelkom/www/recyclerview1/model/Hotel.java
@@ -0,0 +1,15 @@
+package smktelkom.www.recyclerview1.model;
+
+import android.graphics.drawable.Drawable;
+
+public class Hotel {
+    public String judul, deskripsi;
+    public Drawable foto;
+
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
